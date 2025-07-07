b02724ea988d89add24bada88e1594d6e0ef960b
Arthur Kachmazov <arthurkachmazov@yandex.ru>
2025-07-08 02:08:12 +0300

    fix: gh-pages 404

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import ModalCommon from '@/components/ModalWindow/ModalCommon.vue'
 import { useLS } from '@/composables'
 import { LS_KEYS } from '@/constants'
 import AppLayout from '@/layout/AppLayout.vue'
+import router from '@/router'
 import { useAuthStore } from '@/stores/authStore'
 import { storeToRefs } from 'pinia'
 import { onMounted } from 'vue'
@@ -19,6 +20,12 @@ import { RouterView } from 'vue-router'
 const { getStringFromLS } = useLS()
 const authStore = useAuthStore()
 const { isAuthenticated } = storeToRefs(authStore)
+
+// Обработайте query string, если она существует
+const path = window.location.search.split('?')[1]
+if (path) {
+  router.replace(path)
+}
 
 onMounted(() => {
   if (getStringFromLS(LS_KEYS.auth)) {
